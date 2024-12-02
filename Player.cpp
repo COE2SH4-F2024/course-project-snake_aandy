b026fea314e6fc51b2c68ef7fd64246be6e2e70e
hussam89-comp <hussam89@mcmaster.ca>
2024-12-01 22:20:29 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,7 @@
 #include "Player.h"
 
 
-Player::Player(GameMechs* thisGMRef, Food* thisFDRef)
+Player::Player(GameMechs* thisGMRef)
 {
     mainGameMechsRef = thisGMRef;
     playerPosList = new objPosArrayList();
